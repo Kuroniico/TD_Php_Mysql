5caea62b68bbc8b8b450441db3b7ef470372883f
FlorianRabaste <frabaste@la-providence.net>
2020-12-17 12:46:32 +0100

    Restructuration de mon menu

--- fonction.php
+++ fonction.php
@@ -5,125 +5,57 @@
 <html lang="en">
 <link rel="stylesheet" href="style.css">
 <div>
-    <center>
-        <h1>Menu</h1>
-    </center>
-</div>
-<p>
-        <h2>PHP Partie 1</h2>
-    </p>
-    <p>
-        <li><a href=".php"></a></li>
-    </p>
-    <p>
-        <li><a href=".php"></a></li>
-    </p>
-    <p>
-        <li><a href=".php"></a></li>
-    </p>
-    <p>
-        <li><a href=".php"></a></li>
-    </p>
-</div>
-<div>
-    <p>
-        <h2>PHP Partie 2</h2>
-    </p>
-    <p>
-        <li><a href="PHP_Partie2_Exo1.php">Exercice 1</a></li>
-    </p>
-    <p>
-        <li><a href="PHP_Partie2_Exo2.php">Exercice 2</a></li>
-    </p>
-    <p>
-        <li><a href="PHP_Partie2_Exo3.php">Exercice 3</a></li>
-    </p>
-    <p>
-        <li><a href="PHP_Partie2_ExoFinal.php">Exercice Final</a></li>
-    </p>
-</div>
-<div>
-    <p>
-        <h2>BDD</h2>
-    </p>
-    <p>
-        <li><a href="BDD_TD1_Exo1.php">Exercice 1 TP1</a></li>
-    </p>
-    <p>
-        <li><a href="BDD_TD1_Exo2.php">Exercice 2 TP1</a></li>
-    </p>
-    <p>
-        <li><a href="BDD_TD1_Exo3.php">Exercice 3 TP1</a></li>
-    </p>
-    <p>
-        <li><a href="BDD_TD1_Exo4.php">Exercice 4 TP1</a></li>
-    </p>
-    <p>
-        <li><a href="BDD_TD1_Exo5.php">Exercice 5 TP1</a></li>
-    </p>
-    <p>
-        <li><a href="BDD_TD1_Exo6.php">Exercice 6 TP1</a></li>
-    </p>
-    <p>
-        <li><a href="BDD_TD1_Exo7.php">Exercice 7 TP1</a></li>
-    </p>
-    <p>
-        <li><a href="BDD_TD2_Exo1.php">Exercice 1 TP2</a></li>
-    </p>
-    <p>
-        <li><a href="BDD_TD2_Exo2.php">Exercice 2 TP2</a></li>
-    </p>
-    <p>
-        <li><a href="BDD_TD2_Exo3.php">Exercice 3 TP2</a></li>
-    </p>
-</div>
-<div>
-    <p>
-        <h2>SQL</h2>
-    </p>
-    <p>
-        <li><a href="SQL_TD3_Exo2.php">Exercice 2 TD3</a></li>
-    </p>
-    <p>
-        <li><a href="SQL_TD4_Exo1.php">Exercice 1 TD4</a></li>
-    </p>
-    <p>
-        <li><a href="SQL_TD4_Exo2.php">Exercice 2 TD4</a></li>
-    </p>
-    <p>
-        <li><a href="SQL_TD4_Exo3.php">Exercice 3 TD4</a></li>
-    </p>
-</div>
-<div>
-    <p>
-        <h2>HTML</h2>
-    </p>
-    <p>
-        <li><a href="Web/Exo1_HTML.html">Exercice 1</a></li>
-    </p>
-    <p>
-        <li><a href="Web/Exo2_HTML.html">Exercice 2</a></li>
-    </p>
-    <p>
-        <li><a href="Web/Exo3_HTML.html">Exercice 3</a></li>
-    </p>
-    <p>
-        <li><a href="Web/Exo4_HTML.html">Exercice 4</a></li>
-    </p>
-    <p>
-        <li><a href="Web/Exo6_HTML.html">Exercice 6</a></li>
-    </p>
-    <p>
-        <li><a href="Web/TPFinal/index.html">TP Final</a></li>
-    </p>
-</div>
-<div>
-    <p>
-        <h2>CSS</h2>
-    </p>
-    <p>
-        <li><a href="Web/ExoCSS_1.html">Exercice 1</a></li>
-    </p>
+<ul id="menu-demo2">
+	<li><a href="#">PHP Partie 1</a>
+		<ul>
+			<li><a href="#">lien sous menu 1</a></li>
+			<li><a href="#">lien sous menu 1</a></li>
+			<li><a href="#">lien sous menu 1</a></li>
+			<li><a href="#">lien sous menu 1</a></li>
+		</ul>
+	</li>
+	<li><a href="#">PHP Partie 2</a>
+		<ul>
+			<li><a href="PHP_Partie2_Exo1.php">Exercice 1</a></li>
+			<li><a href="PHP_Partie2_Exo2.php">Exercice 2</a></li>
+			<li><a href="PHP_Partie2_Exo3.php">Exercice 3</a></li>
+			<li><a href="PHP_Partie2_ExoFinal.php">Exercice final</a></li>
+		</ul>
+	</li>
+	<li><a href="#">BDD</a>
+		<ul>
+			<li><a href="BDD_TD1_Exo1.php">TD1 Exercice 1</a></li>
+			<li><a href="BDD_TD1_Exo2.php">TD1 Exercice 2</a></li>
+			<li><a href="BDD_TD1_Exo3.php">TD1 Exercice 3</a></li>
+            <li><a href="BDD_TD1_Exo4.php">TD1 Exercice 4</a></li>
+            <li><a href="BDD_TD1_Exo5.php">TD1 Exercice 5</a></li>
+            <li><a href="BDD_TD1_Exo6.php">TD1 Exercice 6</a></li>
+            <li><a href="BDD_TD1_Exo7.php">TD1 Exercice 7</a></li>
+            <li><a href="BDD_TD2_Exo1.php">TD2 Exercice 1</a></li>
+            <li><a href="BDD_TD2_Exo2.php">TD2 Exercice 2</a></li>
+            <li><a href="BDD_TD2_Exo3.php">TD2 Exercice 3</a></li>
+		</ul>
+	</li>
+	<li><a href="#">SQL</a>
+		<ul>
+			<li><a href="SQL_TD3_Exo2.php">TD3 Exercice 2</a></li>
+			<li><a href="SQL_TD4_Exo1.php">TD4 Exercice 1</a></li>
+			<li><a href="SQL_TD4_Exo2.php">TD4 Exercice 2</a></li>
+			<li><a href="SQL_TD4_Exo3.php">TD4 Exercice 3</a></li>
+		</ul>
+    </li>
+    <li><a href="#">HTML</a>
+		<ul>
+			<li><a href="Web/Exo1_HTML.html">HTML Exercice 1</a></li>
+			<li><a href="Web/Exo2_HTML.html">HTML Exercice 2</a></li>
+			<li><a href="Web/Exo3_HTML.html">HTML Exercice 3</a></li>
+            <li><a href="Web/Exo4_HTML.html">HTML Exercice 4</a></li>
+            <li><a href="Web/Exo6_HTML.html">HTML Exercice 6</a></li>
+			<li><a href="Web/ExoCSS_1.html">CSS Exercice 1</a></li>
+			<li><a href="Web/TPFinal/index.html">TP Final</a></li>
+		</ul>
+	</li>
+</ul>
 </div>
 <?php
     }
